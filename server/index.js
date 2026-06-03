997a56ab1725faea6353bebd04166b88a80ed5f8
harsh <harshpatil223@gmail.com>
2026-06-03 23:44:28 +0530

    fixed port configuration

--- server/index.js
+++ server/index.js
@@ -69,7 +69,7 @@ app.get("/api/contact", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
